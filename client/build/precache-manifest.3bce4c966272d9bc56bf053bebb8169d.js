683fdd2703441363f0b0ea7f4dae84e5db5ccf66
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-07-07 18:34:36 +0300

    geploy to heroku

---
+++ client/build/precache-manifest.3bce4c966272d9bc56bf053bebb8169d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f402111e738d1e8644f447cc6021ac49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165a53e097dc11ef8558",
+    "url": "/static/css/main.273d8b7d.chunk.css"
+  },
+  {
+    "revision": "47575532239ab8b4188c",
+    "url": "/static/js/2.e609f58c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e609f58c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165a53e097dc11ef8558",
+    "url": "/static/js/main.a2c596f1.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
